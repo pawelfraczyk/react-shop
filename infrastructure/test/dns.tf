39b92ee37c2d3b5afd95dfd3ee94db03f5097de1
Pawel Fraczyk <pawelfraczyk87@gmail.com>
2022-05-23 21:53:01 +0700

    add-lb-acm

--- infrastructure/test/dns.tf
+++ infrastructure/test/dns.tf
@@ -9,3 +9,15 @@ resource "aws_route53_record" "frontend" {
     evaluate_target_health = true
   }
 }
+
+resource "aws_route53_record" "backend" {
+  allow_overwrite = true
+  name            = "backend.${var.domain}"
+  type            = "A"
+  zone_id         = data.aws_route53_zone.domain.zone_id
+  alias {
+    name                   = aws_lb.public_lb.dns_name
+    zone_id                = aws_lb.public_lb.zone_id
+    evaluate_target_health = true
+  }
+}
